dfec5e9e21c179e000ee4a20462839cd1877876e
kyinn1307 <shruddls1307@gmail.com>
2025-03-09 22:49:01 +0900

    set global layout with module file

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <header>
+        <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+      </header>
+      <main>{children}</main>
+      <footer>제작 @winterlood</footer>
+    </div>
+  );
+}
