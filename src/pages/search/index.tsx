ffbd1b56dc61216eac7a971724f9b5e1d27f0683
kyinn1307 <shruddls1307@gmail.com>
2025-03-26 14:33:07 +0900

    apply ssr page in search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,31 @@
-import { useRouter } from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+import BookItem from "@/components/book-item";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import fetchBooks from "@/lib/fetch-books";
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return {
+    props: { books },
+  };
+};
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
